5c8e12027a110016bca291c1674c3f497c65712e
AJ576 <aditya.jha2020123@gmail.com>
2025-07-31 01:44:16 +0530

    feat: Add build and search functionality for FAISS index

---
+++ build_index.py
@@ -0,0 +1,41 @@
+import faiss
+import json
+import numpy as np
+
+INPUT = "embeddings.jsonl"
+INDEX_FILE = "faiss_index.index"
+META_FILE = "meta.jsonl"
+
+def build_faiss_index():
+    embeddings = []
+    metadata = []
+
+    #this is actually quite simple, we just store embeddings in one array and the data related to it in another array
+    with open(INPUT, "r") as infile:
+        for line in infile:
+            item = json.loads(line)
+            embeddings.append(item["embedding"])
+            metadata.append({
+                "doc_id": item["doc_id"],
+                "chunk_index": item["chunk_index"],
+                "text": item["text"]
+            })
+
+    #then convert the embeddings to a numpy array and create a faiss index
+    embeddings = np.array(embeddings).astype("float32")
+    dim = embeddings.shape[1]
+
+    index = faiss.IndexFlatL2(dim)
+    index.add(embeddings)
+
+    #save the index and metadata to files
+    faiss.write_index(index, INDEX_FILE)
+    with open(META_FILE, "w") as f:
+        for item in metadata:
+            f.write(json.dumps(item) + "\n")
+
+    print(f"Saved index to {INDEX_FILE}")
+    print(f"Saved metadata to {META_FILE}")
+
+if __name__ == "__main__":
+    build_faiss_index()
---
+++ finder.py
@@ -0,0 +1,45 @@
+import faiss
+import json
+import numpy as np
+from sentence_transformers import SentenceTransformer
+
+INDEX_FILE = "faiss_index.index"
+META_FILE = "meta.jsonl"
+MODEL_NAME = "all-MiniLM-L6-v2"
+
+#this just loads the metadata from the metadat file
+def load_metadata():
+    metadata = []
+    with open(META_FILE, "r") as f:
+        for line in f:
+            metadata.append(json.loads(line))
+    return metadata
+
+def search(query, top_k=5):
+    model = SentenceTransformer(MODEL_NAME)
+    query_vector = model.encode(query).astype("float32").reshape(1, -1)  
+
+    index = faiss.read_index(INDEX_FILE)
+    metadata = load_metadata()
+
+    D,I = index.search(query_vector, top_k)
+    results = []
+
+    for idx in I[0]:
+        item = metadata[idx]
+        results.append({
+            "doc_id": item["doc_id"],
+            "chunk_index": item["chunk_index"],
+            "text": item["text"]
+        })
+
+        return results
+    
+    
+if __name__ == "__main__":
+    query = input("Enter your question: ")
+    results = search(query)
+
+    print("\nTop matching chunks:\n")
+    for i, r in enumerate(results, 1):
+        print(f"[{i}] {r['text']}\n")
